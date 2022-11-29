e09cff3c18e704d906f16a7e767a6b85f8815437
Mrunali Manjrekar <mrunali@berkeley.edu>
2022-11-28 23:33:36 -0800

    adding test nb + models.py

--- src/basic_pitch/models.py
+++ src/basic_pitch/models.py
@@ -16,8 +16,12 @@
 # limitations under the License.
 
 from typing import Any, Callable, Dict
-import numpy as np
-import tensorflow as tf
+# import numpy as np
+
+import jax.numpy as jnp
+from flax import linen
+import optax
+
 
 from basic_pitch import nn
 from basic_pitch.constants import (
@@ -31,30 +35,52 @@ from basic_pitch.constants import (
 )
 from basic_pitch.layers import signal, nnaudio
 
-tfkl = tf.keras.layers
+jxln = linen
+# tfkl = tf.keras.layers
+# tfkl = nn.Module
 
-MAX_N_SEMITONES = int(np.floor(12.0 * np.log2(0.5 * AUDIO_SAMPLE_RATE / ANNOTATIONS_BASE_FREQUENCY)))
+MAX_N_SEMITONES = int(jnp.floor(12.0 * jnp.log2(0.5 * AUDIO_SAMPLE_RATE / ANNOTATIONS_BASE_FREQUENCY)))
 
 
-def transcription_loss(y_true: tf.Tensor, y_pred: tf.Tensor, label_smoothing: float) -> tf.Tensor:
+def smoothed_bce_loss(y_true: jnp.ndarray, y_pred: jnp.ndarray, label_smoothing: float) -> jnp.ndarray:
+    """Replication of keras.losses.binary_cross_entropy() in jax, with optax
+    Args:
+        y_true: the true labels
+        y_pred: the predicted labels
+        label_smoothing: Alpha value to smooth values
+    
+    Returns: 
+        The smoothened transcription loss. 
+    
+    """
+    # may need to check whether logits or labels themselves passed here?
+    # also, dimensionality, # of classes, et.c 
+    if label_smoothing > 0:
+        y_pred = optax.smooth_labels(y_pred, label_smoothing)
+
+    bce = optax.softmax_cross_entropy(y_pred, y_true)
+    return bce
+
+
+def transcription_loss(y_true: jnp.ndarray, y_pred: jnp.ndarray, label_smoothing: float) -> jnp.ndarray:
     """Really a binary cross entropy loss. Used to calculate the loss between the predicted
     posteriorgrams and the ground truth matrices.
 
     Args:
         y_true: The true labels.
         y_pred: The predicted labels.
-        label_smoothing: Squeeze labels towards 0.5.
+        label_smoothing: Squeeze labels towards 0.5 or some other value. 
 
     Returns:
         The transcription loss.
     """
-    bce = tf.keras.losses.binary_crossentropy(y_true, y_pred, label_smoothing=label_smoothing)
+    bce = smoothed_bce_loss(y_true=y_true, y_pred=y_pred, label_smoothing=label_smoothing)
     return bce
 
 
 def weighted_transcription_loss(
-    y_true: tf.Tensor, y_pred: tf.Tensor, label_smoothing: float, positive_weight: float = 0.5
-) -> tf.Tensor:
+    y_true: jnp.ndarray, y_pred: jnp.ndarray, label_smoothing: float, positive_weight: float = 0.5
+) -> jnp.ndarray:
     """The transcription loss where the positive and negative true labels are balanced by a weighting factor.
 
     Args:
@@ -66,16 +92,19 @@ def weighted_transcription_loss(
     Returns:
         The weighted transcription loss.
     """
-    negative_mask = tf.equal(y_true, 0)
-    nonnegative_mask = tf.logical_not(negative_mask)
-    bce_negative = tf.keras.losses.binary_crossentropy(
-        tf.boolean_mask(y_true, negative_mask),
-        tf.boolean_mask(y_pred, negative_mask),
+    negative_mask = jnp.equal(y_true, 0)
+    nonnegative_mask = jnp.logical_not(negative_mask)
+    
+
+    # TODO: not sure if converted correctly, may have to check . 
+    bce_negative = smoothed_bce_loss(
+        jnp.where(negative_mask, y_true, 0),
+        jnp.where(negative_mask, y_pred, 1), # 1 allows for loss contribution to be 0?
         label_smoothing=label_smoothing,
     )
-    bce_nonnegative = tf.keras.losses.binary_crossentropy(
-        tf.boolean_mask(y_true, nonnegative_mask),
-        tf.boolean_mask(y_pred, nonnegative_mask),
+    bce_nonnegative = smoothed_bce_loss(
+        jnp.where(nonnegative_mask, y_true, 0),
+        jnp.where(nonnegative_mask, y_pred, 0),
         label_smoothing=label_smoothing,
     )
     return ((1 - positive_weight) * bce_negative) + (positive_weight * bce_nonnegative)
@@ -83,7 +112,7 @@ def weighted_transcription_loss(
 
 def onset_loss(
     weighted: bool, label_smoothing: float, positive_weight: float
-) -> Callable[[tf.Tensor, tf.Tensor], tf.Tensor]:
+) -> Callable[[jnp.ndarray, jnp.ndarray], jnp.ndarray]:
     """
 
     Args:
@@ -125,16 +154,15 @@ def loss(label_smoothing: float = 0.2, weighted: bool = False, positive_weight: 
         "onset": loss_onset,
     }
 
+def _initializer() -> jxln.Initializer:
+    return jxln.initializers.variance_scaling(scale=2.0, mode="fan_avg", distribution="uniform")
 
-def _initializer() -> tf.keras.initializers.VarianceScaling:
-    return tf.keras.initializers.VarianceScaling(scale=2.0, mode="fan_avg", distribution="uniform", seed=None)
-
-
+# TODO: unclear if there is an obvious replication in JAX
 def _kernel_constraint() -> tf.keras.constraints.UnitNorm:
     return tf.keras.constraints.UnitNorm(axis=[0, 1, 2])
 
 
-def get_cqt(inputs: tf.Tensor, n_harmonics: int, use_batchnorm: bool) -> tf.Tensor:
+def get_cqt(inputs: jnp.ndarray, n_harmonics: int, use_batchnorm: bool) -> jnp.ndarray:
     """Calculate the CQT of the input audio.
 
     Input shape: (batch, number of audio samples, 1)
@@ -149,9 +177,9 @@ def get_cqt(inputs: tf.Tensor, n_harmonics: int, use_batchnorm: bool) -> tf.Tens
     Returns:
         The log-normalized CQT of the input audio.
     """
-    n_semitones = np.min(
+    n_semitones = jnp.min(
         [
-            int(np.ceil(12.0 * np.log2(n_harmonics)) + ANNOTATIONS_N_SEMITONES),
+            int(jnp.ceil(12.0 * jnp.log2(n_harmonics)) + ANNOTATIONS_N_SEMITONES),
             MAX_N_SEMITONES,
         ]
     )
@@ -164,9 +192,11 @@ def get_cqt(inputs: tf.Tensor, n_harmonics: int, use_batchnorm: bool) -> tf.Tens
         bins_per_octave=12 * CONTOURS_BINS_PER_SEMITONE,
     )(x)
     x = signal.NormalizedLog()(x)
-    x = tf.expand_dims(x, -1)
+    x = jnp.expand_dims(x, -1)
     if use_batchnorm:
-        x = tfkl.BatchNormalization()(x)
+        bn = jxln.BatchNorm() # TODO: should I normalize a different way?
+        x = bn(x)
+        # x = tfkl.BatchNormalization()(x) # TODO: make sure the initiailzation parameters are all the same
     return x
 
 
@@ -176,7 +206,7 @@ def model(
     n_filters_onsets: int = 32,
     n_filters_notes: int = 32,
     no_contours: bool = False,
-) -> tf.keras.Model:
+) -> nn.Module:
     """Basic Pitch's model implementation.
 
     Args:
@@ -187,7 +217,8 @@ def model(
         no_contours: Whether or not to include contours in the output.
     """
     # input representation
-    inputs = tf.keras.Input(shape=(AUDIO_N_SAMPLES, 1))  # (batch, time, ch)
+    # inputs = tf.keras.Input(shape=(AUDIO_N_SAMPLES, 1))  # (batch, time, ch)
+    inputs = jnp.zeros(shape=(AUDIO_N_SAMPLES, 1)) 
     x = get_cqt(inputs, n_harmonics, True)
 
     if n_harmonics > 1:
@@ -204,90 +235,90 @@ def model(
         )(x)
 
     # contour layers - fully convolutional
-    x_contours = tfkl.Conv2D(
+    x_contours = jxln.Conv(
         n_filters_contour,
         (5, 5),
-        padding="same",
+        padding="SAME",
         kernel_initializer=_initializer(),
-        kernel_constraint=_kernel_constraint(),
+        # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
     )(x)
 
-    x_contours = tfkl.BatchNormalization()(x_contours)
-    x_contours = tfkl.ReLU()(x_contours)
+    x_contours = jxln.BatchNormalization()(x_contours)
+    x_contours = jxln.relu(x_contours)
 
-    x_contours = tfkl.Conv2D(
+    x_contours = jxln.Conv(
         8,
         (3, 3 * 13),
-        padding="same",
+        padding="SAME",
         kernel_initializer=_initializer(),
-        kernel_constraint=_kernel_constraint(),
+        # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
     )(x)
 
-    x_contours = tfkl.BatchNormalization()(x_contours)
-    x_contours = tfkl.ReLU()(x_contours)
+    x_contours = jxln.BatchNorm()(x_contours)
+    x_contours = jxln.relu(x_contours)
 
     if not no_contours:
         contour_name = "contour"
-        x_contours = tfkl.Conv2D(
+        x_contours = jxln.Conv(
             1,
             (5, 5),
-            padding="same",
-            activation="sigmoid",
+            padding="SAME",
             kernel_initializer=_initializer(),
-            kernel_constraint=_kernel_constraint(),
+            # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
             name="contours-reduced",
         )(x_contours)
+        x_contours = jxln.sigmoid(x_contours)
         x_contours = nn.FlattenFreqCh(name=contour_name)(x_contours)  # contour output
 
         # reduced contour output as input to notes
-        x_contours_reduced = tf.expand_dims(x_contours, -1)
+        x_contours_reduced = jnp.expand_dims(x_contours, -1)
     else:
         x_contours_reduced = x_contours
 
-    x_contours_reduced = tfkl.Conv2D(
+    x_contours_reduced = jxln.Conv(
         n_filters_notes,
         (7, 7),
-        padding="same",
+        padding="SAME",
         strides=(1, 3),
         kernel_initializer=_initializer(),
-        kernel_constraint=_kernel_constraint(),
+        # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
     )(x_contours_reduced)
-    x_contours_reduced = tfkl.ReLU()(x_contours_reduced)
+    x_contours_reduced = jxln.relu(x_contours_reduced)
 
     # note output layer
     note_name = "note"
-    x_notes_pre = tfkl.Conv2D(
+    x_notes_pre = jxln.Conv(
         1,
         (7, 3),
-        padding="same",
+        padding="SAME",
         kernel_initializer=_initializer(),
-        kernel_constraint=_kernel_constraint(),
-        activation="sigmoid",
+        # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
     )(x_contours_reduced)
+    x_notes_pre = jxln.sigmoid(x_notes_pre)
     x_notes = nn.FlattenFreqCh(name=note_name)(x_notes_pre)
 
     # onset output layer
 
     # onsets - fully convolutional
-    x_onset = tfkl.Conv2D(
+    x_onset = jxln.Conv(
         n_filters_onsets,
         (5, 5),
-        padding="same",
+        padding="SAME",
         strides=(1, 3),
         kernel_initializer=_initializer(),
-        kernel_constraint=_kernel_constraint(),
+        # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
     )(x)
-    x_onset = tfkl.BatchNormalization()(x_onset)
-    x_onset = tfkl.ReLU()(x_onset)
-    x_onset = tfkl.Concatenate(axis=3, name="concat")([x_notes_pre, x_onset])
-    x_onset = tfkl.Conv2D(
+    x_onset = jxln.BatchNorm()(x_onset)
+    x_onset = jxln.relu(x_onset)
+    x_onset = jnp.concatenate([x_notes_pre, x_onset])
+    x_onset = jxln.Conv(
         1,
         (3, 3),
-        padding="same",
-        activation="sigmoid",
+        padding="SAME",
         kernel_initializer=_initializer(),
-        kernel_constraint=_kernel_constraint(),
+        # kernel_constraint=_kernel_constraint(),  #TODO: fix here once updated
     )(x_onset)
+    x_onset = jxln.sigmoid(x_onset)
 
     onset_name = "onset"
     x_onset = nn.FlattenFreqCh(
@@ -296,4 +327,4 @@ def model(
 
     outputs = {"onset": x_onset, "contour": x_contours, "note": x_notes}
 
-    return tf.keras.Model(inputs=inputs, outputs=outputs)
+    return nn.Module(inputs=inputs, outputs=outputs) # TODO: is this correct? 
