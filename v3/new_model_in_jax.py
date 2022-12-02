ea5e18f9a14e635dcdc2ea3217ec93fdcf9fb9d1
Noah Adhikari <noahadhikari@berkeley.edu>
2022-12-02 00:06:01 -0800

    started working on refactoring

--- v3/new_model_in_jax.py
+++ v3/new_model_in_jax.py
@@ -11,6 +11,55 @@ import pathlib
 # step 1: constant q stacking # Eran?
 # step 2: harmonic stacking # Eran?
 
+class CQTHarmonicStacking(hk.Module):
+    def __init__(self):
+        super().__init__()
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        # step 1: constant q stacking (or is it preprocessed?)
+        # step 2: harmonic stacking
+        return x
+    
+class TopBranch(hk.Module):
+    def __init__(self):
+        super().__init__()
+    def __call__(self, x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
+        x = hk.Conv2D(32, (5, 5), stride=(1, 3))(x)
+        x = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True)(x, is_training=is_training)
+        x = jax.nn.relu(x)
+        return x
+
+class YpBranch(hk.Module):
+    def __init__(self):
+        super().__init__()
+    def __call__(self, x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
+        x = hk.Conv2D(16, (5, 5))(x)
+        x = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True)(x, is_training=is_training)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(8, (3, 39))(x)
+        x = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True)(x, is_training=is_training)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(1, (5, 5))(x)
+        x = jax.nn.sigmoid(x)
+        return x
+
+class YnBranch(hk.Module):
+    def __init__(self):
+        super().__init__()
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        x = hk.Conv2D(32, (7, 7), stride=(1, 3))(x)
+        x = jax.nn.relu(x)
+        x = hk.Conv2D(1, (7, 3))(x)
+        x = jax.nn.sigmoid(x)
+        return x
+
+class YoBranch(hk.Module):
+    def __init__(self):
+        super().__init__()
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        x = hk.Conv2D(1, (3, 3))(x)
+        x = jax.nn.sigmoid(x)
+        return x
+    
 class PosteriorgramModel(hk.Module):
 
     def __init__(self):
@@ -21,65 +70,69 @@ class PosteriorgramModel(hk.Module):
         #     # harmonic stacking layer
         #     hk.IdentityCore()  # TODO: replace with actual signal processing
         # ])
-        self.cqt_harmonic_stacking = hk.Linear(1)
+        # self.cqt_harmonic_stacking = hk.Linear(1)
         
-        self.top_branch = hk.Sequential([
-            # 32-depth Conv2D (5x5), stride 1x3
-            # batch norm
-            # ReLU
-            hk.Conv2D(32, (5, 5), stride=(1, 3)),
-            # hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True),
-            jax.nn.relu
-        ])
-        self.yp_branch = hk.Sequential([
-            # downward prong:
-            # 16 x 5 x 5 Conv 2d
-            # Batch Norm
-            # ReLU
-            # 8 x 3 x39 Conv 2D
-            # Batch Norm, RELU
-            # 1 Conv 2D, 5x5, sigmoid
+        # self.top_branch = hk.Sequential([
+        #     # 32-depth Conv2D (5x5), stride 1x3
+        #     # batch norm
+        #     # ReLU
+        #     hk.Conv2D(32, (5, 5), stride=(1, 3)),
+        #     # hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True),
+        #     jax.nn.relu
+        # ])
+        # self.yp_branch = hk.Sequential([
+        #     # downward prong:
+        #     # 16 x 5 x 5 Conv 2d
+        #     # Batch Norm
+        #     # ReLU
+        #     # 8 x 3 x39 Conv 2D
+        #     # Batch Norm, RELU
+        #     # 1 Conv 2D, 5x5, sigmoid
 
-            hk.Conv2D(16, (5, 5)),
-            # hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True),
-            jax.nn.relu,
-            hk.Conv2D(8, (3, 39)),
-            # hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True),
-            jax.nn.relu,
-            hk.Conv2D(1, (5, 5)),
-            jax.nn.sigmoid
-        ])
-        self.yn_branch = hk.Sequential([
-            # 32 conv 2d 7x7 stride 1x3
-            # relu
-            # 1 conv2d 7x3
-            # sigmoid
-            hk.Conv2D(32, (7, 7), stride=(1, 3)),
-            jax.nn.relu,
-            hk.Conv2D(1, (7, 3)),
-            jax.nn.sigmoid
-        ])
-        self.yo_branch = hk.Sequential([
-            # 1 conv2d 3x3
-            # sigmoid
-            hk.Conv2D(1, (3, 3)),
-            jax.nn.sigmoid
-        ])
+        #     hk.Conv2D(16, (5, 5)),
+        #     # hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True),
+        #     jax.nn.relu,
+        #     hk.Conv2D(8, (3, 39)),
+        #     # hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True),
+        #     jax.nn.relu,
+        #     hk.Conv2D(1, (5, 5)),
+        #     jax.nn.sigmoid
+        # ])
+        # self.yn_branch = hk.Sequential([
+        #     # 32 conv 2d 7x7 stride 1x3
+        #     # relu
+        #     # 1 conv2d 7x3
+        #     # sigmoid
+        #     hk.Conv2D(32, (7, 7), stride=(1, 3)),
+        #     jax.nn.relu,
+        #     hk.Conv2D(1, (7, 3)),
+        #     jax.nn.sigmoid
+        # ])
+        # self.yo_branch = hk.Sequential([
+        #     # 1 conv2d 3x3
+        #     # sigmoid
+        #     hk.Conv2D(1, (3, 3)),
+        #     jax.nn.sigmoid
+        # ])
 
     # define all model layers here as attributes
 
-    def __call__(self, audio) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    def __call__(self, audio: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
         # preprocessed = self.cqt_harmonic_stacking(audio)
-        yp = self.yp_branch(audio)
-        yn = self.yn_branch(yp)
-        top = self.top_branch(audio)
-        concat = jax.numpy.concatenate([top, yn], axis=2)  # TODO: fix this probably
-        yo = self.yo_branch(concat)
-        print(yp, yn, yo)
+        # yp = self.yp_branch(audio)
+        # yn = self.yn_branch(yp)
+        # top = self.top_branch(audio)
+        # concat = jax.numpy.concatenate([top, yn], axis=2)  # TODO: fix this probably
+        # yo = self.yo_branch(concat)
+        # print(yp, yn, yo)
+        
+        preprocessed = CQTHarmonicStacking()(audio)
+        
+        
         return yp, yn, yo
 
-def onset_posteriorgram_model(audio):  # Shalin, Mrunali
-    pass
+# def onset_posteriorgram_model(audio):  # Shalin, Mrunali
+#     pass
 # def right_prong_up_to_concat():
 # # step 3: 32-depth Conv2D (5x5), stride 1x3
     # step 4: batch norm
