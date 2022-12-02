b75f28c0b6408a42e847e04f70ac8156a2703b0f
Noah Adhikari <noahadhikari@berkeley.edu>
2022-12-02 00:35:21 -0800

    still refactoring

--- v3/new_model_in_jax.py
+++ v3/new_model_in_jax.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Dict, Tuple
+from typing import Union, List, Dict, Tuple, Optional
 import numpy as np
 import jax
 import jax.numpy as jnp
@@ -12,59 +12,72 @@ import pathlib
 # step 2: harmonic stacking # Eran?
 
 class CQTHarmonicStacking(hk.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: Optional[str] = None):
+        super().__init__(name=name)
     def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
         # step 1: constant q stacking (or is it preprocessed?)
         # step 2: harmonic stacking
         return x
     
 class TopBranch(hk.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: Optional[str] = None):
+        super().__init__(name=name)
+        self.conv32_5_5 = hk.Conv2D(32, (5, 5), stride=(1, 3), name="conv32_5_5")
+        self.bn = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn")
+        
     def __call__(self, x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
-        x = hk.Conv2D(32, (5, 5), stride=(1, 3))(x)
-        x = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True)(x, is_training=is_training)
+        x = self.conv(x)
+        x = self.bn(x, is_training=is_training)
         x = jax.nn.relu(x)
         return x
 
 class YpBranch(hk.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: Optional[str] = None):
+        super().__init__(name=name)
+        self.conv16_5_5 = hk.Conv2D(16, (5, 5), name="conv16_5_5")
+        self.bn1 = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn1")
+        self.conv8_3_39 = hk.Conv2D(8, (3, 39), name="conv8_3_39")
+        self.bn2 = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn2")
+        self.conv_1_5_5 = hk.Conv2D(1, (5, 5), name="conv_1_5_5")
+        
     def __call__(self, x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
-        x = hk.Conv2D(16, (5, 5))(x)
-        x = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True)(x, is_training=is_training)
+        x = self.conv16_5_5(x)
+        x = self.bn1(x, is_training=is_training)
         x = jax.nn.relu(x)
-        x = hk.Conv2D(8, (3, 39))(x)
-        x = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True)(x, is_training=is_training)
+        x = self.conv8_3_39(x)
+        x = self.bn2(x, is_training=is_training)
         x = jax.nn.relu(x)
-        x = hk.Conv2D(1, (5, 5))(x)
+        x = self.conv_1_5_5(x)
         x = jax.nn.sigmoid(x)
         return x
 
 class YnBranch(hk.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: Optional[str] = None):
+        super().__init__(name=name)
+        self.conv32_7_7 = hk.Conv2D(32, (7, 7), stride=(1, 3), name="conv32_7_7")
+        self.conv1_7_3 = hk.Conv2D(1, (7, 3), name="conv1_7_3")
+        
     def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
-        x = hk.Conv2D(32, (7, 7), stride=(1, 3))(x)
+        x = self.conv32_7_7(x)
         x = jax.nn.relu(x)
-        x = hk.Conv2D(1, (7, 3))(x)
+        x = self.conv1_7_3(x)
         x = jax.nn.sigmoid(x)
         return x
 
 class YoBranch(hk.Module):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, name: Optional[str] = None):
+        super().__init__(name=name)
+        self.conv1_3_3 = hk.Conv2D(1, (3, 3), name="conv1_3_3")
+        
     def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
-        x = hk.Conv2D(1, (3, 3))(x)
+        x = self.conv1_3_3(x)
         x = jax.nn.sigmoid(x)
         return x
     
 class PosteriorgramModel(hk.Module):
 
-    def __init__(self):
-        # define blocks here:
-        super().__init__()
+    def __init__(self, name: Optional[str] = None):
+        super().__init__(name=name)
         # self.cqt_harmonic_stacking = hk.Sequential([
         #     # cqt layer
         #     # harmonic stacking layer
@@ -114,10 +127,13 @@ class PosteriorgramModel(hk.Module):
         #     hk.Conv2D(1, (3, 3)),
         #     jax.nn.sigmoid
         # ])
+        self.cqt_harmonic_stacking = CQTHarmonicStacking()
+        self.top_branch = TopBranch()
+        self.yp_branch = YpBranch()
+        self.yn_branch = YnBranch()
+        self.yo_branch = YoBranch()
 
-    # define all model layers here as attributes
-
-    def __call__(self, audio: jnp.ndarray) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
+    def __call__(self, audio: jnp.ndarray, is_training: bool) -> Tuple[jnp.ndarray, jnp.ndarray, jnp.ndarray]:
         # preprocessed = self.cqt_harmonic_stacking(audio)
         # yp = self.yp_branch(audio)
         # yn = self.yn_branch(yp)
@@ -126,9 +142,12 @@ class PosteriorgramModel(hk.Module):
         # yo = self.yo_branch(concat)
         # print(yp, yn, yo)
         
-        preprocessed = CQTHarmonicStacking()(audio)
-        
-        
+        processed = self.cqt_harmonic_stacking(audio)
+        yp = self.yp_branch(processed, is_training)
+        yn = self.yn_branch(yp)
+        top = self.top_branch(processed, is_training)
+        concat = jax.numpy.concatenate([top, yn], axis=2) 
+        yo = self.yo_branch(concat)
         return yp, yn, yo
 
 # def onset_posteriorgram_model(audio):  # Shalin, Mrunali
