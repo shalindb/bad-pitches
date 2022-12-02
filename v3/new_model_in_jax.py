fixed concat issue, bn is definitely the problem, not sure how to fix the average error

v3/new_model_in_jax.py
@@ -23,11 +23,11 @@ class TopBranch(hk.Module):
     def __init__(self, name: Optional[str] = None):
         super().__init__(name=name)
         self.conv32_5_5 = hk.Conv2D(32, (5, 5), stride=(1, 3), name="conv32_5_5")
-        self.bn = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn")
+        # self.bn = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn")
         
     def __call__(self, x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
         x = self.conv32_5_5(x)
-        x = self.bn(x, is_training=is_training)
+        # x = self.bn(x, is_training=is_training)
         x = jax.nn.relu(x)
         return x
 
@@ -35,17 +35,17 @@ class YpBranch(hk.Module):
     def __init__(self, name: Optional[str] = None):
         super().__init__(name=name)
         self.conv16_5_5 = hk.Conv2D(16, (5, 5), name="conv16_5_5")
-        self.bn1 = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn1")
+        # self.bn1 = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn1")
         self.conv8_3_39 = hk.Conv2D(8, (3, 39), name="conv8_3_39")
-        self.bn2 = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn2")
+        # self.bn2 = hk.BatchNorm(decay_rate=0.9, create_scale=True, create_offset=True, name="bn2")
         self.conv_1_5_5 = hk.Conv2D(1, (5, 5), name="conv_1_5_5")
         
     def __call__(self, x: jnp.ndarray, is_training: bool) -> jnp.ndarray:
         x = self.conv16_5_5(x)
-        x = self.bn1(x, is_training=is_training)
+        # x = self.bn1(x, is_training=is_training)
         x = jax.nn.relu(x)
         x = self.conv8_3_39(x)
-        x = self.bn2(x, is_training=is_training)
+        # x = self.bn2(x, is_training=is_training)
         x = jax.nn.relu(x)
         x = self.conv_1_5_5(x)
         x = jax.nn.sigmoid(x)
@@ -89,6 +89,6 @@ class PosteriorgramModel(hk.Module):
         yp = self.yp_branch(processed, is_training)
         yn = self.yn_branch(yp)
         top = self.top_branch(processed, is_training)
-        concat = jax.numpy.concatenate([top, yn], axis=2) 
+        concat = jax.numpy.concatenate([top, yn], axis=-1) 
         yo = self.yo_branch(concat)
         return yp, yn, yo